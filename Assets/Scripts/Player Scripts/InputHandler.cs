901e40ef365dbf68f1c9f2a28f647ffe96813ff8
LemonScentedAss <whitapat1999@gmail.com>
2022-10-04 19:22:03 -0500

    More test stuff in my scene with Cinemachine

--- Assets/Scripts/Player Scripts/InputHandler.cs
+++ Assets/Scripts/Player Scripts/InputHandler.cs
@@ -6,14 +6,27 @@ public class InputHandler : MonoBehaviour
 {
       public Vector2 inputVector { get; private set; }
       public Vector3 mousePosition { get; private set; }
+      public bool zoomedIn { get; private set; }
 
       // Update is called once per frame
       void Update()
       {
+         // Movement
             var h = Input.GetAxis("Horizontal");
             var v = Input.GetAxis("Vertical");
             inputVector = new Vector2(h, v);
 
             mousePosition = Input.mousePosition;
+
+        // Camera Zoom
+            if (zoomedIn && Input.GetKeyDown(KeyCode.Z))
+                zoomedIn = false;
+            if (!zoomedIn && Input.GetKeyDown(KeyCode.Z))
+                zoomedIn = true;            
       }
+
+    private void Start()
+    {
+        zoomedIn = true;
+    }
 }
