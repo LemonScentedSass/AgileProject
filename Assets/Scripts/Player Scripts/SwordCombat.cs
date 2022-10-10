d67329db122d5906c33e662446cf1001d5a4fb81
LemonScentedAss <whitapat1999@gmail.com>
2022-10-10 09:38:06 -0500

    Added sword combat

--- Assets/Scripts/Player Scripts/SwordCombat.cs
+++ Assets/Scripts/Player Scripts/SwordCombat.cs
@@ -9,27 +9,83 @@ public class SwordCombat : MonoBehaviour
     public float cooldownTime = 2f;
     private float nextFireTime = 0f;
     public static int noOfClicks = 0;
-    float lastClickedFrame = 0;
+    float LastClickedTime = 0;
     float maxComboDelay = 1;
 
 
     void Start()
     {
-        anim = GetComponent<Animator>();
+        anim = GetComponentInChildren<Animator>();
         input = GetComponent<InputHandler>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(input.swordKey)
+        if (anim.GetCurrentAnimatorStateInfo(1).normalizedTime > 0.7f
+                && anim.GetCurrentAnimatorStateInfo(1).IsName("Attack 1"))
         {
-            lastClickedFrame = Time.time;
-            noOfClicks++;
-            if(noOfClicks == 1)
-            {
+            anim.SetBool("attack1", false);
+        }
 
+        if (anim.GetCurrentAnimatorStateInfo(1).normalizedTime > 0.7f
+                && anim.GetCurrentAnimatorStateInfo(1).IsName("Attack 2"))
+        {
+            anim.SetBool("attack2", false);
+        }
+
+        if (anim.GetCurrentAnimatorStateInfo(1).normalizedTime > 0.7f
+                && anim.GetCurrentAnimatorStateInfo(1).IsName("Attack 3"))
+        {
+            anim.SetBool("attack3", false);
+            noOfClicks = 0;
+        }
+
+        if (Time.time - LastClickedTime > maxComboDelay)
+        {
+            noOfClicks = 0;
+        }
+
+        if (noOfClicks == 0)
+        {
+            anim.SetBool("attack1", false);
+            anim.SetBool("attack2", false);
+            anim.SetBool("attack3", false);
+        }
+
+
+        if (Time.time > nextFireTime)
+        {
+
+            if (input.swordKey)
+            {
+                LastClickedTime = Time.time;
+                noOfClicks++;
+                if (noOfClicks == 1)
+                {
+                    anim.SetBool("attack1", true);
+                }
+                noOfClicks = Mathf.Clamp(noOfClicks, 0, 3);
+
+                if (noOfClicks >= 2 && anim.GetCurrentAnimatorStateInfo(1).normalizedTime > 0.7f
+                    && anim.GetCurrentAnimatorStateInfo(1).IsName("Attack 1"))
+                {
+                    anim.SetBool("attack1", false);
+                    anim.SetBool("attack2", true);
+                }
+
+                if (noOfClicks >= 3 && anim.GetCurrentAnimatorStateInfo(1).normalizedTime > 0.7f
+                    && anim.GetCurrentAnimatorStateInfo(1).IsName("Attack 2"))
+                {
+                    anim.SetBool("attack2", false);
+                    anim.SetBool("attack3", true);
+                }
             }
         }
+    }
+
+    public void Test()
+    {
+
     }
 }
