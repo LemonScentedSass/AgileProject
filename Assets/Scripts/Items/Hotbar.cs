4fb8b70038983e98e8e2f1882ae2d74bb0d67bea
LemonScentedAss <whitapat1999@gmail.com>
2022-10-30 18:34:33 -0500

    walls tag

--- Assets/Scripts/Items/Hotbar.cs
+++ Assets/Scripts/Items/Hotbar.cs
@@ -239,7 +239,7 @@ public class Hotbar : MonoBehaviour
         while (t < duration)
         {
             Debug.Log("trigger");
-            Debug.Log(reset);
+            //Debug.Log(reset);
             t += Time.deltaTime;
             reset = t / duration;
             fill.fillAmount = reset;
