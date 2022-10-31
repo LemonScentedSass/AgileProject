3249b26f145aad4d9b8cd81803bce13915a3e4c0
LemonScentedAss <whitapat1999@gmail.com>
2022-10-30 23:22:17 -0500

    Its just a burning memory

--- Assets/CrateBreak.cs
+++ Assets/CrateBreak.cs
@@ -6,13 +6,40 @@ public class CrateBreak : MonoBehaviour, IHittable
 {
     public int crateHealth = 1;
 
+    [SerializeField] bool isBroken;
+
+    ResourceDropper resourceDropper;
+
+    private void Start()
+    {
+        resourceDropper = GetComponentInChildren<ResourceDropper>();
+    }
+
+    private void Update()
+    {
+        if (isBroken)
+        {
+            Break();
+        }
+    }
+
     public void GetHit(int damage)
     {
-        crateHealth -= damage;
-        if (crateHealth <= 0)
+        if (isBroken == false)
         {
-            Destroy(gameObject);
+            crateHealth -= damage;
+
+            if (crateHealth <= 0)
+            {
+                isBroken = true;
+            }
         }
+    }
+
+    private void Break()
+    {
+        resourceDropper.DropItem();
+        Destroy(gameObject);
     }
 
     public void GetStunned(float length)
