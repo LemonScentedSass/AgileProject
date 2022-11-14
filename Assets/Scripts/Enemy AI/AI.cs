77d674a4b898ba9ed40c17c5ebe9a7c6a0fe7029
Nick Rhodes <ncrhodes12@gmail.com>
2022-11-14 11:47:03 -0600

    AI Tweaks and Anim Changes
    - AI now stops when entering attack state
    - large enemies have slower anims
    - large enemies have different attack and death anims

--- Assets/Scripts/Enemy AI/AI.cs
+++ Assets/Scripts/Enemy AI/AI.cs
@@ -5,27 +5,29 @@ using UnityEngine.AI;
 
 public class AI : MonoBehaviour
 {
-      NavMeshAgent agent;
-      Animator anim;
-      public Transform player;
-      public State currentState;
+    NavMeshAgent agent;
+    Animator anim;
+    public Transform player;
+    public State currentState;
 
-      private void Start()
-      {
-            agent = this.GetComponent<NavMeshAgent>();
-            anim = this.GetComponent<Animator>();
-            currentState = new Idle(this.gameObject, agent, anim, player);
-      }
+    [SerializeField] public float attackRange = 2f;
 
-      private void Update()
-      {
-            currentState = currentState.Process();
+    private void Start()
+    {
+        agent = this.GetComponent<NavMeshAgent>();
+        anim = this.GetComponent<Animator>();
+        currentState = new Idle(this.gameObject, agent, anim, player);
+    }
 
-            if (agent == null)                              // ??????????
-            {
-                  currentState.Exit();
-            }
-      }
+    private void Update()
+    {
+        currentState = currentState.Process();
+
+        if (agent == null)                              // ??????????
+        {
+            currentState.Exit();
+        }
+    }
 
 
 }
