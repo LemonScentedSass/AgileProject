0c178a7ffc3c656004dcc624e7b021a7b7f83fa5
CNachb <colinnach@gmail.com>
2023-02-05 21:06:26 -0600

    Arm Animations

--- Assets/NewMenuInputTest.cs
+++ Assets/NewMenuInputTest.cs
@@ -7,6 +7,9 @@ public class NewMenuInputTest : MonoBehaviour
     public Animator HandsAnimator;
     public Animator MenuBookAnimator;
 
+    public bool Mirrored;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,24 +19,47 @@ public class NewMenuInputTest : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(Mirrored == true)
+        {
+            HandsAnimator.SetBool("Mirrored", true);
+        }
+        else
+        {
+            HandsAnimator.SetBool("Mirrored", false);
+        }
+
         if(Input.GetKeyDown(KeyCode.Tab) == true)
         {
             MenuBookAnimator.Play("BookOpening");
             HandsAnimator.Play("OpenBookMenu", 2);
         }
-        if(Input.GetKeyDown(KeyCode.Escape) == true)
+        if(Mirrored == false && Input.GetKeyDown(KeyCode.Escape) == true)
         {
             MenuBookAnimator.Play("New State");
             HandsAnimator.Play("LeftArmMagicIdle", 2);
         }
-        if(Input.GetMouseButtonDown(1) == true)
+
+        if(Mirrored == false && Input.GetKeyDown(KeyCode.M))
+        {
+            Mirrored = true;
+        }
+        else if( Mirrored == true == Input.GetKeyDown(KeyCode.M))
+        {
+            Mirrored = false;
+        }
+
+        if(Mirrored == false && Input.GetMouseButtonDown(1) == true)
         {
             HandsAnimator.Play("LeftArmMagicShoot", 2);
+        }
+        else if(Mirrored == true && Input.GetMouseButtonDown(0)==true)
+        {
+            HandsAnimator.Play("RightArmMagicShoot", 1);
         }
 
 
         //Right Hand weapon swings on click
-        if(Input.GetMouseButtonDown(0) == true)
+        if(Input.GetMouseButtonDown(0) == true && Mirrored == false)
         {
             //Checks to see if the player is in idle; Plays attack 1
             if(HandsAnimator.GetCurrentAnimatorStateInfo(1).IsName("RightArmWeaponLeftNothing-Idle"))
@@ -55,12 +81,44 @@ public class NewMenuInputTest : MonoBehaviour
             {
                 HandsAnimator.SetBool("Final", true);
             }
+        }
 
-            //Being in idle and attacking again makes final hit bool reset
-            if (HandsAnimator.GetCurrentAnimatorStateInfo(1).IsName("RightArmWeaponLeftNothing-Idle"))
+        //Being in idle and attacking again makes final hit bool reset
+        if (HandsAnimator.GetCurrentAnimatorStateInfo(1).IsName("RightArmWeaponLeftNothing-Idle"))
+        {
+            HandsAnimator.SetBool("Final", false);
+        }
+
+        //Mirrored input
+        if (Input.GetMouseButtonDown(1) == true && Mirrored == true)
+        {
+            //Checks to see if the player is in idle; Plays attack 1
+            if (HandsAnimator.GetCurrentAnimatorStateInfo(2).IsName("LeftArmWeaponRightNothing-Idle"))
             {
-                HandsAnimator.SetBool("Final", false);
+                HandsAnimator.Play("LeftArmWeaponRightNothing-Attack1", 2);
             }
+
+            //Checks to see if player is in attack1 then activates next attack
+            if (HandsAnimator.GetCurrentAnimatorStateInfo(2).IsName("LeftArmWeaponRightNothing-Attack1"))
+            {
+                HandsAnimator.SetBool("isAttacking", true);
+
+                //Resets the bool that transitioned the first attack to the second
+                StartCoroutine(AttackWait(.5f));
+            }
+
+            //Checks to see if in attack2; Set bool to start final hit
+            if (HandsAnimator.GetCurrentAnimatorStateInfo(2).IsName("LeftArmWeaponRightNothing-Attack2"))
+            {
+                HandsAnimator.SetBool("Final", true);
+            }
+
+        }
+
+        //Being in idle and attacking again makes final hit bool reset
+        if (HandsAnimator.GetCurrentAnimatorStateInfo(2).IsName("LeftArmWeaponRightNothing-Attack3"))
+        {
+            HandsAnimator.SetBool("Final", false);
         }
 
     }
