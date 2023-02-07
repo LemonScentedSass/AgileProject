9b483bf42569e79facf33c86606f82edb23561bd
Nick Rhodes <simplytrekly@gmail.com>
2023-02-07 01:29:27 -0600

    Effect System cont.
    Pushing changes to effect system, still incomplete

--- Assets/Scripts/Best Items Ever/EffectSystem/TimedEffect.cs
+++ Assets/Scripts/Best Items Ever/EffectSystem/TimedEffect.cs
@@ -5,24 +5,27 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Effect/Timed Effect")]
 public class TimedEffect : EffectBase
 {
-    public EffectBase[] onTick;
+      public EffectBase[] onTick;
 
-    public int duration;
+      public float duration;
+      public int damagePerTick;
+      [SerializeField] public ParticleSystem effectParticles;
 
-    public override string GetEffectType { get { return "TimedEffect"; } }
+      public override string GetEffectType { get { return "TimedEffect"; } }
 
-    public override void OnTrigger(Character source, Character target)
-    {
-        throw new System.NotImplementedException();
-    }
+      public override void OnTrigger(Character source, Character target)
+      {
+            ParticleSystem ps = Instantiate(effectParticles, target.transform.position, Quaternion.identity);
+            target.gameObject.GetComponent<EnemyStats>().GetHit(damagePerTick);
+      }
 
-    public void OnTick(Character source, Character target)
-    {
-        Debug.Log("Tick Yo!");
+      public void OnTick(Character source, Character target)
+      {
+            Debug.Log("Tick Yo!");
 
-        for (int i = 0; i < onTick.Length; i++)
-        {
-            onTick[i].OnTrigger(source, target);
-        }
-    }
+            for (int i = 0; i < onTick.Length; i++)
+            {
+                  onTick[i].OnTrigger(source, target);
+            }
+      }
 }
